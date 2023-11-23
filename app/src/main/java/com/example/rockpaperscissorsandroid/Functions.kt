81dbaff1a3e4d987f9203337cd63203d50cb9d73
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-23 12:41:31 +0100

    .

---
+++ app/src/main/java/com/example/rockpaperscissorsandroid/Functions.kt
@@ -0,0 +1,4 @@
+package com.example.rockpaperscissorsandroid
+
+class Functions {
+}
--- app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.rockpaperscissorsandroid
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,21 +44,40 @@ fun GamePage(modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        IconButton(onClick = { /*TODO*/ }, modifier = modifier.size(100.dp).rotate(40F)) {
-            Icon(painter = painterResource(id = R.drawable.scissors), contentDescription = "scissors", tint = Color(R.drawable.scissors))
-        }
-        IconButton(onClick = { /*TODO*/ }, modifier = modifier.size(100.dp)) {
-            Icon(painter = painterResource(id = R.drawable.paper), contentDescription = "scissors", tint = Color(R.drawable.paper))
-        }
-        IconButton(onClick = { /*TODO*/ }, modifier = modifier.size(100.dp)) {
-            Icon(painter = painterResource(id = R.drawable.stone), contentDescription = "scissors", tint = Color(R.drawable.stone))
-        }
+        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+            content = {
+                Image(painter = painterResource(id = R.drawable.scissors),
+                    contentDescription = "scissors",
+                    modifier = modifier
+                        .size(100.dp)
+                        .rotate(40F)
+                )
+            }
+        )
+        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+            content = {
+                Image(painter = painterResource(id = R.drawable.paper),
+                    contentDescription = "paper",
+                    modifier = modifier
+                        .size(90.dp)
+                )
+            }
+        )
+        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+            content = {
+                Image(painter = painterResource(id = R.drawable.stone),
+                    contentDescription = "stone",
+                    modifier = modifier
+                        .size(100.dp)
+                )
+            }
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun test() {
+fun Test() {
     RockPaperScissorsAndroidTheme {
         GamePage()
     }
