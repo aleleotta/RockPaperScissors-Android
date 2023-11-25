9307b242e84285f07147e5b3ec959a48eebd00c3
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-25 23:29:48 +0100

    .

--- app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +42,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GamePage(modifier: Modifier = Modifier) {
     Row (
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().background(Color.Cyan),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
