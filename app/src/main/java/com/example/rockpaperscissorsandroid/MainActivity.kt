0ad7d8f5814aa7ef2ebddbb12dca211f339f1195
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-26 00:28:47 +0100

    .

--- app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsandroid/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,8 +20,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rockpaperscissorsandroid.ui.theme.RockPaperScissorsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RockPaperScissorsAndroidTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -41,35 +45,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GamePage(modifier: Modifier = Modifier) {
-    Row (
-        modifier = modifier.fillMaxSize().background(Color.Cyan),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
-            content = {
-                Image(painter = painterResource(id = R.drawable.stone),
-                    contentDescription = "stone",
-                    modifier = modifier.size(100.dp)
-                )
-            }
-        )
-        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
-            content = {
-                Image(painter = painterResource(id = R.drawable.paper),
-                    contentDescription = "paper",
-                    modifier = modifier.size(90.dp)
-                )
-            }
-        )
-        TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
-            content = {
-                Image(painter = painterResource(id = R.drawable.scissors),
-                    contentDescription = "scissors",
-                    modifier = modifier.size(100.dp).rotate(40F)
-                )
-            }
-        )
+        Row (
+            verticalAlignment = Alignment.Vertical { size, space -> 250 }
+        ) {
+            Text(
+                text = "Pick an element!",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Medium,
+            )
+        }
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+                content = {
+                    Image(painter = painterResource(id = R.drawable.stone),
+                        contentDescription = "stone",
+                        modifier = modifier.size(100.dp)
+                    )
+                }
+            )
+            TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+                content = {
+                    Image(painter = painterResource(id = R.drawable.paper),
+                        contentDescription = "paper",
+                        modifier = modifier.size(90.dp)
+                    )
+                }
+            )
+            TextButton(onClick = { /*TODO*/ }, modifier = modifier.size(120.dp),
+                content = {
+                    Image(painter = painterResource(id = R.drawable.scissors),
+                        contentDescription = "scissors",
+                        modifier = modifier
+                            .size(100.dp)
+                            .rotate(40F)
+                    )
+                }
+            )
+        }
     }
 }
 
